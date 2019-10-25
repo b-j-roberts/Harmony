8d6b9f11dd2f2be91def64b32533b7d99ced7b98
Brandon, the Master <brandonjroberts22@gmail.com>
2019-10-25 17:23:20 -0500

    config reader begin

--- config-reader/reader.h
+++ config-reader/reader.h
@@ -2,7 +2,26 @@
 
 #include <sting>
 #include <vector>
+#include <map>
+#include <ifstream>
+#include <stdexcept>
 
+#include <boost/algorithm/string/trim.hpp>
+
+// TO DO : Namespaces
+
+// Class to detect beginning and ending parenthesis in string
+class Parenthesis_Detect {
+
+  size_t count;
+
+public:
+
+  Parentheis_Detect(): count(0) { }
+
+}
+
+// Requires { } , ( ), [ ] if on more than one line
 class Parameter {
 
   // TO DO : Think about name index of this param? maybe use a map?
@@ -21,18 +40,42 @@ public:
   float as_float() { return std::stof(value); }                    // stof
   std::string as_string() { return value; }                        // string
   // TO DO : Make template version as using lexical cast?
+  // TO DO : As vector
+  //         As Map ?
+  //         As queue ?
 
 };
 
 class Config_Reader {
 
-  std::vector<Parameter> params_; // All parameters in top level of given config file
+  std::map<std::string, Config_Reader> params_; // Map from names of Config Readers to Config Readers in top level of file
 
   // TO DO : NON-COPYABLE
+  //
+
+  // Config_Reader from a string
+  Config_Reader(const std::string& str, bool private_mask) { }
 
 public:
 
-  Config_Reader(const std::string& filename) { /* TO DO : load file, parse parameters */ }
+  // Config_Reader from a file
+  Config_Reader(const std::string& filename) { 
+    std::ifstream config_in(filename);
+    if(config_in.is_open()) {
+      std::string curr_line;
+      while(getline(config_in, curr_line)) {
+        size_t delim_pos = curr_line.find(':');
+        if(delim_pos != std::string::npos) {
+          std::string key = curr_line.substr(0, delim_pos);
+          boost::trim(key);
+          // get value using parenthesis detect
+          // insert into map
+        }
+      }
+    } else {
+      throw std::runtime_error("Error opening config file : " + filename);
+    }
+  }
 
   // TO DO : Has function to tell if file has parameter of passed name
 
