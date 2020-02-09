2e18f036877b7a053bd02c21b49f783f6e120510
b-j-roberts <brandonjroberts22@gmail.com>
2020-02-09 16:47:32 +0900

    Cleaned and slight reworks to arg_parser & config-reader

--- arg_parser/template_version/arg_parser.h
+++ arg_parser/template_version/arg_parser.h
@@ -1,176 +1,116 @@
-#include <stdexcept>
-
-#include <vector>
 #include <string>
-#include <memory>
 #include <map>
+
+#include <stdexcept> // runtime_error
+#include <memory> // shared_ptr & dynamic_pointer_cast
 
 #include <boost/lexical_cast.hpp>
 
-// TO DO : Introduce namespaces? harmony { } & parser? { } & impl namespace ? 
-// TO DO : Think about if people might want to access argument desc or help function/string
 // TO DO : Think about constructor name convention, '-id' & '--name'
-// TO DO : static_cast to dynamic_cast with nullptr check?
+// TO DO : Interface for -h? flag to show help_string after fully loaded?
+// TO DO : Check for illegitamite flags?
 
 
-// Implimentation
+// Implimentation Things ( Helpers )
 
 // Return Type of argument's value (specialized for strings)
 template<typename T>
 struct arg_ret { typedef const T type; };
-
 template<>
 struct arg_ret<std::string> { typedef const std::string& type; };
 
-// Base class pointer used for argument
-// This is so arguement can have general template type
+// Base class pointer used for argument ( allowing template value inside map )
 class argument_base {
-
-  // NON COPYABLE
-  argument_base(const argument_base&);
-  argument_base& operator=(const argument_base&);
-
-  const std::string desc;
-
 public:
-
-  argument_base(std::string&& d): desc(std::move(d)) { }
-  
+  argument_base() = default;
+  argument_base(const argument_base&) = delete;
+  argument_base& operator=(const argument_base&) = delete;
+  virtual ~argument_base() = default;
 };
 
 // Class holding value of argument of type T
 template<typename T>
 class argument : public argument_base {
-
-  // NON COPYABLE
-  argument(const argument&);
-  argument& operator=(const argument&);
-
   T value_;
-
 public:
-
-  argument(T val, std::string&& d): argument_base(std::move(d)), value_(val) { }
-
+  explicit argument(T val): value_(val) { }
+  ~argument() override = default;
   typename arg_ret<T>::type value() const { return value_; }
-
 };
-// End of implimentation items
 
+
+// Interface Class
 
 // Command line argument parser modelled after arg_parse in python
 class Parser {
 
-  std::vector<std::unique_ptr<argument_base>> args_; // Holds list of arg values if passed
-  const std::string desc_;                           // Description of parser or program
-  std::map<std::string, size_t> name_to_pos_;        // Maps name in add_argument to pos in args_
-  bool parsed;                                       // if parse() function called (required)
+  // Maps name of argument (given in add_argument) to argument value ( by ptr to base class )
+  std::map<std::string, std::shared_ptr<argument_base>> name_to_arg_;
 
-  // pre-parser struct which loads all passed params like '-id argument' or '--name argument' into
-  // a map, also holds help_string for help function and if we should show help upon calling parse()
-  struct Pre_Parser {
-    
-    std::map<std::string, std::string> pre_parsed_;
-    void clear() { pre_parsed_.clear(); }
+  // String used to diplay help information : called when error thrown
+  std::string help_string;
+  inline static const std::string help_str_spaces = "           "; // Formatting spaces
 
-    std::string help_string;
-    bool show_help;
-
-    // constructor does pre-parsing
-    //   TO DO : Think about ways to not show_help if argument starts with '-'
-    //   TO DO : Think about what if name and id given in list of flags
-    Pre_Parser(int argc, char** argv, const std::string& s): help_string(s + '\n'), show_help(false) {
-      for(size_t i = 1; i < argc - 1; ++i) {
-        if(argv[i][0] == '-') {
-          if(pre_parsed_.count(argv[i]) == 0) {
-            pre_parsed_[argv[i]] = argv[i+1];
-          } else {
-            help_string += "           Make sure not to use same flag twice!\n";
-            show_help = true; 
-          }
-        }
-      }
-    }
-
-  } pre_;
-
-
-  // NON COPYABLE
-  Parser(const Parser&);
-  Parser& operator=(const Parser&); 
-
-  void help() {
-    throw std::runtime_error(pre_.help_string); 
-  }
-
-  // get value at passed idx in args_
-  template<typename T>
-  typename arg_ret<T>::type get(size_t idx) {
-    argument<T>* tmp = static_cast<argument<T>*>(args_[idx].get());
-    std::unique_ptr<argument<T>> derived_ptr;
-    args_[idx].release();
-    derived_ptr.reset(tmp);
-    typename arg_ret<T>::type val = derived_ptr->value();
-    return val;
-  }
+  // Pre-parsed args <id/name, value> ( done at construction before add_arguments have been called )
+  // TO DO : Think of way to clear this after done loading name_to_arg_
+  std::map<std::string, std::string> pre_parsed_;
 
 public:
 
-  explicit Parser(int argc, char** argv):  pre_(argc, argv, ""), parsed(false) { }
-  explicit Parser(int argc, char** argv, const std::string& s):  
-    pre_(argc, argv, s), 
-    desc_(s), 
-    parsed(false) { }
-
-  // Function to add an argument to list of possible arguments
-  // This function also appends argument to args_ if it was pre-parsed
-  template<typename T>
-  void add_argument(const std::string& id, const std::string& name, 
-                    std::string&& desc, bool required = false) {
-    pre_.help_string += "           " + id + "  " + name + "  " + desc + 
-                        "  " + (required ? "(required)\n" : "(not required)\n"); 
-    if(pre_.pre_parsed_.count(id)) {
-      name_to_pos_[name.substr(2)] = args_.size();
-      T val = boost::lexical_cast<T>(pre_.pre_parsed_[id]);
-      args_.emplace_back(std::make_unique<argument<T>>(val, std::forward<std::string>(desc)));
-    } else if(pre_.pre_parsed_.count(name)) {
-      name_to_pos_[name.substr(2)] = args_.size();
-      T val = boost::lexical_cast<T>(pre_.pre_parsed_[name]);
-      args_.emplace_back(std::make_unique<argument<T>>(val, std::forward<std::string>(desc)));
-    } else {
-      if(required) {
-        pre_.help_string += "           Required argument : " + name.substr(2) + " not given!\n";
-        pre_.show_help = true;
+  Parser(int argc, char** argv, const std::string& desc = ""):
+    help_string(desc + '\n') {
+    // Do Pre-Parsing ( fills pre_parsed_ )
+    // TO DO : Think about ways to let values start with '-'
+    // TO DO : Think about if flag doesnt have value ( ie -v for verbose mode )
+    for(int i = 1; i < argc - 1; ++i) {
+      if(argv[i][0] == '-') {
+        if(pre_parsed_.count(argv[i]) == 0) {
+          pre_parsed_[argv[i]] = argv[i+1];
+        } else {
+          help_string += help_str_spaces + "Make sure not to use same flag twice!\n";
+          throw std::runtime_error(help_string); 
+        }
       }
     }
   }
 
-  // Makes sure everything has been delt with properly and makes parser ready to work
-  void parse() {
-    if(pre_.show_help) help();
-    pre_.clear();
-    parsed = true;
+  Parser(const Parser&) = delete;
+  Parser& operator=(const Parser&) = delete; 
+
+  // Appends argument to name_to_args_ if it was pre-parsed ( & requirement handling )
+  template<typename T>
+  void add_argument(const std::string& id, const std::string& name, 
+                    const std::string& desc, bool required = false) {
+    // TO DO : Show type info ( make names readable )
+    help_string += help_str_spaces + id + "   " + name + "   " + desc + 
+                   "  " + (required ? "(required)\n" : "(not required)\n"); 
+    // TO DO : Error if pre_parsed id & name passed
+    if(pre_parsed_.count(id)) {
+      T val = boost::lexical_cast<T>(pre_parsed_[id]);
+      name_to_arg_.emplace(name.substr(2), std::make_shared<argument<T>>(val));
+    } else if(pre_parsed_.count(name)) {
+      T val = boost::lexical_cast<T>(pre_parsed_[name]);
+      name_to_arg_.emplace(name.substr(2), std::make_shared<argument<T>>(val));
+    } else if(required) {
+      help_string += help_str_spaces + "Required argument '" + name.substr(2) + "' not given!\n";
+      throw std::runtime_error(help_string); 
+    }
   }
 
-  // Function to get argument corresponding to passed name
-  // Requires calling parse() before use
+  // If 'name' has corresponding add_argument entry & was a passed in argument
+  bool has(const std::string& name) { return name_to_arg_.count(name); }
+
+  // Get argument value corresponding to passed name
   template<typename T>
   typename arg_ret<T>::type get(const std::string& name) {
-    if(!parsed) {
-      pre_.help_string += "           Please make sure to call parse function before get/has!\n";
-      help();
-    }
-    if(name_to_pos_.count(name)) {
-      return get<T>(name_to_pos_[name]); // TO DO : Check for if name exists and check if parsed
+    if(name_to_arg_.count(name)) {
+      // Downcast Base pointer
+      auto derived_ptr = std::dynamic_pointer_cast<argument<T>>(name_to_arg_[name]);
+      // TO DO : if(derived_ptr) ?
+      return derived_ptr->value();
     } else {
-      pre_.help_string += "           Indexing argument '" + name + "' which is not given!\n";
-      help(); 
+      help_string += help_str_spaces + "Indexed argument '" + name + "' not given!\n";
+      throw std::runtime_error(help_string);
     }
   }
-
-  // Function which returns true if the argument is in args_ (ie add_argument and passed in)
-  bool has(const std::string& name) {
-    return name_to_pos_.count(name);
-  }
-
 };
--- config-reader/config_reader.h
+++ config-reader/config_reader.h
@@ -1,292 +1,213 @@
 #include <string>
-#include <vector>
 #include <map>
-#include <fstream>
-#include <stdexcept>
-#include <sstream>
-#include <utility>
+#include <vector> // for as_vector() function
+#include <array> // for bracer_pair's
+#include <utility> // pair
+
+#include <fstream> // istream
+#include <sstream> // istringstream
+#include <stdexcept> // runtime_error
 
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/lexical_cast.hpp>
 
-// TO DO : Namespaces
-// TO DO : Split into cpp file, Look into static library creation
-// TO DO : Think about returning config map and param map options
-//         (so one can for loop over all configs/params)
-// TO DO : Defaultable parameter
 // TO DO : Think about ';' for termination
-// TO DO : include reasons
+// TO DO : Way to comment out parts of config
+// TO DO : Think string_view instead of string
 
-// Stores actual parameter values, and has as_* functions to obtain this value
+// Stores string a key's value from a config file/string
+// Impliments as_* function to obtain string as different types
 class Parameter {
 
   const std::string value;
 
 public:
 
-  // Copy constructors and assignment operators taken as default
-
   explicit Parameter(const std::string& s): value(s) { } 
   Parameter() = default;
 
   // as_* functions to get parameter value
-  const int as_int() const { return std::stoi(value); }                        // stoi
-  const int as_int(int base) const { return std::stoi(value, nullptr, base); } // stoi with base
-  const double as_double() const { return std::stod(value); }                  // stod
-  const float as_float() const { return std::stof(value); }                    // stof
-  const std::string& as_string() const { return value; }                       // string
-
+  int as_int() const { return std::stoi(value); }                        // stoi
+  int as_int(int base) const { return std::stoi(value, nullptr, base); } // stoi with base
+  double as_double() const { return std::stod(value); }                  // stod
+  float as_float() const { return std::stof(value); }                    // stof
+  const std::string& as_string() const { return value; }                 // string
   template <typename T> const T as() const { return boost::lexical_cast<T>(value); } // template
 
-  // if parameter is in format item1, item2, ...
-  const std::vector<Parameter> as_vector() const {
-    std::string curr_val;
-    std::vector<Parameter> ret;
-    std::istringstream reader(value);
-    while(getline(reader, curr_val, ',')) {
-      boost::trim(curr_val);
-      ret.emplace_back(curr_val);
-    }
-    return ret;
-  }
-
-  // if parameter is in format item1 : this, item2 : that, ...
-  std::map<std::string, Parameter> as_map() const {
-    std::string curr_val;
-    std::map<std::string, Parameter> ret;
-    std::istringstream reader(value);
-    while(getline(reader, curr_val, ',')) {
-      size_t sep = curr_val.find(':');
-      std::string key = curr_val.substr(0, sep);
-      boost::trim(key);
-      std::string val = curr_val.substr(sep + 1); 
-      boost::trim(val);
-      ret.emplace(key, Parameter(val));
-    }
-    return ret;
-  }
-
+  // Return vector of parameters if value of form : val1, val2, ...
+  const std::vector<Parameter> as_vector() const;
+  // Return map of parameters if value of form : key1 : val1, key2 : val2, ...
+  std::map<std::string, Parameter> as_map() const;
 };
 
-
-// Returns end bracer of passed char, if invalid returns '\001'
-std::pair<char, char> is_valid_bracer(char c) {
-  static const size_t delim_num{6};
-  static char valid_inner_start[delim_num] = { '{', '[', '\'', '(', '\"', '<' };
-  static char valid_inner_end[delim_num] = { '}', ']', '\'', ')', '\"', '>' };
-  for(size_t i = 0; i < delim_num; ++i) {
-    if(c == valid_inner_start[i]) 
-      return std::make_pair(valid_inner_start[i], valid_inner_end[i]);
+const std::vector<Parameter> Parameter::as_vector() const {
+  std::string curr_val;
+  std::vector<Parameter> ret;
+  std::istringstream reader(value);
+  while(getline(reader, curr_val, ',')) {
+    boost::trim(curr_val);
+    ret.emplace_back(curr_val);
   }
-  return std::make_pair('\001', '\001');
+  return ret;
 }
 
-// Main object that parses config files and stores file's params
+std::map<std::string, Parameter> Parameter::as_map() const {
+  std::string curr_val;
+  std::map<std::string, Parameter> ret;
+  std::istringstream reader(value);
+  while(getline(reader, curr_val, ',')) {
+    size_t sep = curr_val.find(':');
+    std::string key = curr_val.substr(0, sep);
+    boost::trim(key);
+    std::string val = curr_val.substr(sep + 1); 
+    boost::trim(val);
+    ret.emplace(key, Parameter(val));
+  }
+  return ret;
+}
+
+
+// Return bracer pair for c if it is valid, otherwise returns pair of '\001'
+const std::pair<char, char>& is_valid_bracer(char c) {
+  static const size_t delim_num = 6;
+  static const std::array<std::pair<char, char>, delim_num> valid_bracers = 
+     {{ {'{', '}'}, {'[', ']'}, {'\'', '\''}, {'(', ')'}, {'\"', '\"'}, {'<', '>'} }};
+  static const std::pair<char, char> invalid_pair = { '\001', '\001' };
+  for(const auto& bracer : valid_bracers) if(c == bracer.first) return bracer;
+  return invalid_pair;
+}
+
+// Object containing nested Parameters & Config_Readers ( sections contained in { } after a key ) 
+// Also the overall file given in constructor is it's own Config_Reader
 class Config_Reader {
 
-  std::map<std::string, Config_Reader> configs_; // Map from names of Config Readers to 
-                                                 // Config Readers in top level of this
-  std::map<std::string, Parameter> params_;      // Map from parameter name to values in this 
+  // Key to Config_Reader map ( top level config readers inside this config reader )
+  std::map<std::string, Config_Reader> configs_;
+  // Key to Parameter map ( of form : 'key : value\n' in the containing Config_Reader )
+  std::map<std::string, Parameter> params_;
 
-  // Config_Reader from a string
-  Config_Reader(const std::string& str, const std::string& filename) { 
-    std::istringstream config_in(str);
-    std::string curr_line;
-    while(getline(config_in, curr_line)) {
-      size_t delim_pos = curr_line.find(':');
-      if(delim_pos != std::string::npos) {
-        std::string key = curr_line.substr(0, delim_pos);
-        boost::trim(key);
-
-        // get value using parenthesis detection
-        bool obtained = false;
-        size_t pos = delim_pos + 1;
-        bool begin_accumulate = false;
-        size_t first_acc = 0;
-        size_t accumulator = 0;
-        std::pair<char, char> bracer_pair;
-        std::string value;
-        do {
-          size_t line_len = curr_line.length();
-          for(;pos < line_len; ++pos) {
-            if(!begin_accumulate && curr_line[pos] != ' ') {
-              bracer_pair = is_valid_bracer(curr_line[pos]);
-              begin_accumulate = true;
-              accumulator = 1;
-              if(bracer_pair.first == '\001') { // it is line, no bracers
-                value = curr_line.substr(pos);
-                boost::trim(value);
-                obtained = true;
-                break;
-              } else {
-                ++pos; // get inside
-                first_acc = pos;
-                if(pos >= line_len) continue;
-              }
-            }
-            if(begin_accumulate) {
-              if(curr_line[pos] == bracer_pair.second) --accumulator;
-              if(accumulator == 0) {
-                obtained = true;
-                value += curr_line.substr(first_acc, pos - first_acc);
-                first_acc = 0;
-              }
-              if(curr_line[pos] == bracer_pair.first) ++accumulator;
-            }
-          }
-          pos = 0;
-          if(first_acc != 0) {
-            value += curr_line.substr(first_acc) + '\n';
-            first_acc = 0;
-          } else if(!obtained) value += curr_line + '\n';
-        } while(!obtained && getline(config_in, curr_line));
-
-        if(!obtained) throw std::runtime_error("Some end bracket is missing in : " + filename);
-
-        // insert into map
-        boost::trim(value);
-        if(bracer_pair.first == '{') {
-          // this is a value that needed to be kept
-          if(configs_.count(key)) throw std::runtime_error(key + " included more than once!");
-          this->configs_.emplace(key, Config_Reader(value, filename));
-        } else {
-          // this is another config reader that needs to be constructed from string constructor
-          if(params_.count(key)) throw std::runtime_error(key + " included more than once!");
-          this->params_.emplace(key, Parameter(value));
-        }
-      }
-    }
-  } // end of constructor
+  // Config_Reader from a string ( for nested Config_Readers )
+  Config_Reader(const std::string& str, const std::string& filename);
+  // Parse a stream type for Config_Readers & Parameters ( used to construct a Config_Reader )
+  void parse_config_stream(std::istream& s, const std::string& filename); 
 
 public:
 
-  //Using default copy const and assignment operator
+  // Create Config_Reader from a file w/ path 'filename'
+  explicit Config_Reader(const std::string& filename);
 
-  Config_Reader() = default;
-
-  // Config_Reader from a file
-  explicit Config_Reader(const std::string& filename) { 
-    std::ifstream config_in(filename);
-    if(config_in.is_open()) {
-      std::string curr_line;
-      while(getline(config_in, curr_line)) {
-        size_t delim_pos = curr_line.find(':');
-        if(delim_pos != std::string::npos) {
-          std::string key = curr_line.substr(0, delim_pos);
-          boost::trim(key);
-
-          // get value using parenthesis detection
-          bool obtained = false;
-          size_t pos = delim_pos + 1;
-          bool begin_accumulate = false;
-          size_t first_acc = 0;
-          size_t accumulator = 0;
-          std::pair<char, char> bracer_pair;
-          std::string value;
-          do {
-            size_t line_len = curr_line.length();
-            for(;pos < line_len; ++pos) {
-              if(!begin_accumulate && curr_line[pos] != ' ') {
-                bracer_pair = is_valid_bracer(curr_line[pos]);
-                begin_accumulate = true;
-                accumulator = 1;
-                if(bracer_pair.first == '\001') { // it is line, no bracers
-                  value = curr_line.substr(pos);
-                  boost::trim(value);
-                  obtained = true;
-                  break;
-                } else {
-                  ++pos; // get inside
-                  first_acc = pos;
-                  if(pos >= line_len) continue;
-                }
-              }
-              if(begin_accumulate) {
-                if(curr_line[pos] == bracer_pair.second) --accumulator;
-                if(accumulator == 0) {
-                  obtained = true;
-                  value += curr_line.substr(first_acc, pos - first_acc);
-                  first_acc = 0;
-                }
-                if(curr_line[pos] == bracer_pair.first) ++accumulator;
-              }
-            }
-            pos = 0;
-            if(first_acc != 0) {
-              value += curr_line.substr(first_acc) + '\n';
-              first_acc = 0;
-            } else if(!obtained) value += curr_line + '\n';
-          } while(!obtained && getline(config_in, curr_line));
-         
-          if(!obtained) throw std::runtime_error("Some end bracket is missing in : " + filename);
-
-          // insert into map
-          boost::trim(value);
-          if(bracer_pair.first == '{') {
-            // this is a value that needed to be kept
-            if(configs_.count(key)) throw std::runtime_error(key + " included more than once!");
-            this->configs_.emplace(key, Config_Reader(value, filename));
-          } else {
-            // this is another config reader that needs to be constructed from string constructor
-            if(params_.count(key)) throw std::runtime_error(key + " included more than once!");
-            this->params_.emplace(key, Parameter(value));
-          }
-        }
-      }
-    } else {
-      throw std::runtime_error("Error opening config file : " + filename);
-    }
-  } // end of main constructor
-
-  // Access param using param name or nested :: stucture name
-  const Parameter& param(const std::string& param) const {
-    if(params_.count(param)) {
-      return params_.find(param)->second;
-    } else {
-      size_t pos = param.find("::");
-      std::vector<std::string> param_list;
-      if(pos != std::string::npos) param_list.push_back(param.substr(0, pos));
-      while(pos != std::string::npos) {
-        size_t new_pos = param.find("::", pos + 2);
-        param_list.push_back(param.substr(pos + 2, new_pos - pos - 2));
-        pos = new_pos;
-      }
-      const Config_Reader* nester = this;
-      for(size_t i = 0, i_end = param_list.size(); i < i_end; ++i) {
-        if(i != i_end - 1) nester = &nester->config(param_list[i]);
-        else return nester->param(param_list[i]);
-      }
-      throw std::out_of_range("Parameter : " + param + " not included!");
-    }
-  }
-
-  // indexing params
-  const Parameter& operator[] (const std::string& param_) const {
-    return param(param_);
-  }
-
-  // indexing nested config readers
+  // Returns whether this Config_Reader contains Config_Reader w/ key 'name'
+  // TO DO : Allow checking nested Config_Readers like Parameters ?
+  bool has_config(const std::string& name) const { return configs_.count(name); }
+  // Index configs_ map for Config_Reader
+  // TO DO : Is this ever really needed ?
   const Config_Reader& config(const std::string& name) const {
     if(configs_.count(name)) {
-      return configs_.find(name)->second;
+      return configs_.at(name);
     } else {
-      throw std::out_of_range("Config Reader : " + name + " not included!");
+      throw std::runtime_error("Config Reader : " + name + " not included!");
     }
   }
 
-  // Detects if param_ is accessable param object
-  bool has_param(const std::string& param_) const {
-    try {
-      auto test = param(param_);
-    }
-    catch(std::out_of_range) {
-      return false;
-    }
+  // nested syntax : "conf_1::conf_2::conf_3::...::param_name"
+  //   this indexing configs_ w/ key conf_1 => indexed Config_Reader's configs_ w/ key conf_2 => ...
+  //   till last param_name which indexes last Config_Reader's params_ w/ key param_name
+
+  // Returns whether there is a Parameter w/ key 'name' ( using nested syntax )
+  bool has_param(const std::string& name) const {
+    // Uses functionality of param(name) to determine if accessable w/ nested syntax
+    try { (void) param(name); } catch(...) { return false; }
     return true;
   }
-
-  bool has_config(const std::string& name) const {
-    return configs_.count(name);
-  }
-
+  // Index Parameter w/ nested syntax
+  const Parameter& param(const std::string& param_path) const;
+  const Parameter& operator[] (const std::string& param_path) const { return param(param_path); }
 };
+
+void Config_Reader::parse_config_stream(std::istream& s, const std::string& filename) {
+  std::string curr_line;
+  while(getline(s, curr_line)) {
+    // Find key/value seperator
+    size_t delim_pos = curr_line.find(':');
+    if(delim_pos != std::string::npos) {
+      // get key
+      std::string key = curr_line.substr(0, delim_pos);
+      boost::trim(key);
+      
+      // get value
+      std::string value;
+      curr_line = curr_line.substr(delim_pos + 1);
+      bool obtained = false;
+      bool accumulating = false;
+      int accumulator = 0;
+      std::pair<char, char> bracer_pair;
+      do {
+        for(const auto& c : curr_line) {
+          if(accumulating) {
+            if(c == bracer_pair.second) --accumulator;
+            if(accumulator == 0) {
+              obtained = true;
+              break;
+            }
+            if(c == bracer_pair.first) ++accumulator;
+          } else {
+            if(c != ' ') {
+              bracer_pair = is_valid_bracer(c);
+              accumulating = true;
+              ++accumulator;
+              if(bracer_pair.first == '\001') {
+                obtained = true;
+                break;
+              }
+            }
+          } 
+        }
+        value += curr_line + '\n';
+      } while(!obtained && getline(s, curr_line));
+      if(!obtained) throw std::runtime_error("Some end bracer or value is missing in : " + filename);
+
+      if(bracer_pair.first != '\001') {
+        size_t first_brace = value.find(bracer_pair.first);
+        value = value.substr(first_brace + 1, value.rfind(bracer_pair.second) - first_brace - 1);
+      }
+      boost::trim(value);
+
+      // Insert key/value into Config_Reader or Parameter based on bracer type
+      if(bracer_pair.first == '{') {
+        if(configs_.count(key)) throw std::runtime_error(key + " included more than once!");
+        this->configs_.emplace(key, Config_Reader(value, filename));
+      } else {
+        if(params_.count(key)) throw std::runtime_error(key + " included more than once!");
+        this->params_.emplace(key, Parameter(value));
+      }
+    }
+  }
+}
+
+
+Config_Reader::Config_Reader(const std::string& str, const std::string& filename) { 
+  std::istringstream config_in(str);
+  parse_config_stream(config_in, filename);
+}
+
+Config_Reader::Config_Reader(const std::string& filename) { 
+  std::ifstream config_in(filename);
+  if(config_in.is_open()) {
+    parse_config_stream(config_in, filename);
+  } else {
+    throw std::runtime_error("Error opening config file : " + filename);
+  }
+}
+
+const Parameter& Config_Reader::param(const std::string& name) const {
+  if(params_.count(name)) {
+    return params_.at(name);
+  } else {
+    size_t pos = name.find("::");
+    if(pos == std::string::npos) throw std::runtime_error("Parameter : " + name + " not included!");
+    if(configs_.count(name.substr(0, pos))) 
+      return configs_.at(name.substr(0, pos)).param(name.substr(pos + 2));
+    else throw std::runtime_error("No Config matching name : " + name.substr(0, pos));
+  }
+}
+
