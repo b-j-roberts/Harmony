4a48df5374c621b77174299ccd1ca3d0b289aad7
Brandon, the Master <brandonjroberts22@gmail.com>
2020-04-08 13:32:00 -0500

    vector_visual README.mkd

--- sfml/vector_visual/visualize.cpp
+++ sfml/vector_visual/visualize.cpp
@@ -29,18 +29,33 @@ int main() {
     window.clear();
 
     // Call Visualization function
-    // linear_Range_Shader(window, visualize, Jet_Shader());
-    // linear_Range_Shader(window, visualize, Jet_Shader(), -.1f, .1f);
-    // linear_Range_Shader(window, visualize, Jet_Shader(), -.25f, .25f);
-    // linear_Range_Shader_Mesh(window, visualize, Grayscale_Shader(), 10, -.25f, .25f);
-    // linear_Range_Shader_Dots(window, visualize, Purple_Shader(), 10, 0.f, 1.f);
 
-    make_Or_Break(window, visualize, .15f, sf::Color::Red);
+    // 1 )
+    // linear_Range_Shader(window, visualize, Jet_Shader());
+    
+    // 2 )
+    // linear_Range_Shader(window, visualize, Jet_Shader(), -.1f, .1f);
+    
+    // 3 )
+    // linear_Range_Shader(window, visualize, Jet_Shader(), -.25f, .25f);
+    
+    // 4 )
+    // linear_Range_Shader_Mesh(window, visualize, Grayscale_Shader(), 10, -.25f, .25f);
+    
+    // 5 )
+     linear_Range_Shader_Dots(window, visualize, Purple_Shader(), 10, 0.f, 2.f);
+
+    // 6 )
+    // make_Or_Break(window, visualize, .15f, sf::Color::Red);
+
     window.display();
   }
 
 }
+
 /*
+// 1 )
+
 std::vector<std::vector<float>> get_Vector() {
   std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
   const float scale = (window_size.y - 1) * (window_size.x - 1);
@@ -55,6 +70,8 @@ std::vector<std::vector<float>> get_Vector() {
 */
 
 /*
+// 2 )
+
 std::vector<std::vector<float>> get_Vector() {
   std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
   for(int y = 0; auto& row : ret) {
@@ -67,20 +84,10 @@ std::vector<std::vector<float>> get_Vector() {
   return ret;
 }
 */
+
 /*
-std::vector<std::vector<float>> get_Vector() {
-  std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
-  for(float y = 0.f; auto& row : ret) {
-    for(float x = 0.f; auto& elem : row) {
-      elem = std::sin(x + window_size.x / 2.f) * std::cos(y + window_size.y / 2.f) / 10.f;
-      x+= .01;
-    }
-    y+=.01;
-  }
-  return ret;
-}
-*/
-/*
+// 3 )
+
 std::vector<std::vector<float>> get_Vector() {
   std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
   for(float y = 0.f; auto& row : ret) {
@@ -95,6 +102,23 @@ std::vector<std::vector<float>> get_Vector() {
 */
 
 /*
+// 4 )
+
+std::vector<std::vector<float>> get_Vector() {
+  std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
+  for(float y = 0.f; auto& row : ret) {
+    for(float x = 0.f; auto& elem : row) {
+      elem = std::sin(x + window_size.x / 2.f) * std::cos(y + window_size.y / 2.f) / 10.f;
+      x+= .01;
+    }
+    y+=.01;
+  }
+  return ret;
+}
+*/
+
+// 5 )
+
 std::vector<std::vector<float>> get_Vector() {
   std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
   for(float y = 0.f; auto& row : ret) {
@@ -106,7 +130,9 @@ std::vector<std::vector<float>> get_Vector() {
   }
   return ret;
 }
-*/
+
+/*
+// 6 )
 
 std::vector<std::vector<float>> get_Vector() {
   std::vector<std::vector<float>> ret(window_size.y, std::vector<float>(window_size.x));
@@ -119,4 +145,4 @@ std::vector<std::vector<float>> get_Vector() {
   }
   return ret;
 }
-
+*/
